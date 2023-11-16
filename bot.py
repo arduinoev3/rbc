94f375fec2b40795d3312638a38dc0f027bff504
Arduinoev3 <v2072211@yandex.ru>
2023-11-16 20:50:03 +0300

    book added

--- bot.py
+++ bot.py
@@ -2,14 +2,23 @@ import telebot
 from telebot import types
 from collections import deque
 from dataclasses import dataclass
-token="6851790018:AAFmSMFLj0VQr5xLkxSWk_lC45Fb7__RL48"
-chanel_name = "test_talks_chanel"
+import os
+from io import BytesIO
+
+if os.environ.get('API_KEY'):
+    token = os.environ.get('API_KEY')
+else:
+    token="6851790018:AAFmSMFLj0VQr5xLkxSWk_lC45Fb7__RL48"
+if os.environ.get('CHANEL_NAME'):
+    chanel_name = os.environ.get('CHANEL_NAME')
+else:
+    chanel_name = "test_talks_chanel"
 admins = ["arduinoev3", "derskov"]
 
 @dataclass
 class User:
     id: int
-    step: int = 0
+    step: int
     name: str
     first: str
     last: str
@@ -22,7 +31,7 @@ bot=telebot.TeleBot(token)
 @bot.message_handler(commands=['start'])
 def start_message(message):
     global m
-    m[message.from_user.username] = User(id=message.chat.id, name=message.from_user.username, first=message.from_user.first_name, last=message.from_user.last_name, phone=None)
+    m[message.from_user.username] = User(id=message.chat.id, step=0, name=message.from_user.username, first=message.from_user.first_name, last=message.from_user.last_name, phone=None)
     bot.send_message(message.chat.id, f"Привет! Напиши, пожалуйста, свой номер телефона")
 
 @bot.message_handler(commands=['get'])
@@ -31,30 +40,28 @@ def start_message(message):
     if message.from_user.username in admins:
         bot.send_message(message.chat.id, "\n".join([m[it].__repr__() for it in m]))
 
-@bot.message_handler(commands=['check_subscription'])
-def check_subscription(message):
-    global chanel_name
-    user_id = message.chat.id
- 
-    try:
-        member = bot.get_chat_member("@" + chanel_name, user_id)
-        if member.status == 'member' or member.status == 'creator':
-            bot.reply_to(message, 'Вы подписаны на канал')
-        else:
-            bot.reply_to(message, 'Вы не подписаны на канал')
-    except Exception as e:
-        bot.reply_to(message, str(e))
-
 @bot.message_handler()
 def start_message(message):
-    global m
+    global m, chanel_name
     if message.from_user.username in m:
         if m[message.from_user.username].step == 0:
             m[message.from_user.username].phone = message.text
             m[message.from_user.username].step = 1
-            bot.send_message(m[message.from_user.username].id, "Еще раз приветсвую - зацени https://publictalk.rbc.ru. (Если хочешь получить в подарок финансовый дневник, то подписывайся на канал)")
+            bot.send_message(m[message.from_user.username].id, f"Еще раз приветсвую - зацени https://publictalk.rbc.ru. (Если хочешь получить в подарок финансовый дневник, то подписывайся на канал https://t.me/{chanel_name})")
         else:
-            pass
+            user_id = message.chat.id
+            try:
+                member = bot.get_chat_member("@" + chanel_name, user_id)
+                if member.status == 'member' or member.status == 'creator':
+                    m[message.from_user.username].step = 2
+                    bot.reply_to(message, 'Вы подписаны на канал, кидаю дневник')
+
+                    f = open("Финансовый дневник.pdf","rb")
+                    bot.send_document(message.chat.id,f)
+                else:
+                    bot.reply_to(message, f'Вы не подписаны на канал, подпишитесь https://t.me/{chanel_name}')
+            except Exception as e:
+                bot.send_message(m["arduinoev3"].id, str(e))
     else:
         bot.send_message(message.chat.id, "Тебя не в игре - введи /start")
 
