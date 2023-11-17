44332dc5edc198c366b7bd02115930547d7f17b1
Arduinoev3 <v2072211@yandex.ru>
2023-11-17 10:25:41 +0300

    polling mode changed

--- bot.py
+++ bot.py
@@ -30,41 +30,33 @@ bot=telebot.TeleBot(token)
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    global m
-    m[message.from_user.username] = User(id=message.chat.id, step=0, name=message.from_user.username, first=message.from_user.first_name, last=message.from_user.last_name, phone=None)
+    #global m
+    #m[message.from_user.username] = User(id=message.chat.id, step=0, name=message.from_user.username, first=message.from_user.first_name, last=message.from_user.last_name, phone=None)
     bot.send_message(message.chat.id, f"Привет! Мы проводим https://publictalk.rbc.ru")
 
 @bot.message_handler(commands=['get'])
 def start_message(message):
-    global m, admins
+    #global m, admins
     if message.from_user.username in admins:
         bot.send_message(message.chat.id, "\n".join([m[it].__repr__() for it in m]))
 
+@bot.message_handler(commands=['check_me'])
+def start_message(message):
+    try:
+        member = bot.get_chat_member("@" + check_name, message.chat.id)
+        if member.status == 'member' or member.status == 'creator':
+            bot.reply_to(message, 'Вы с нами, кидаю дневник')
+
+            f = open("Финансовый дневник.pdf","rb")
+            bot.send_document(message.chat.id,f)
+        else:
+            bot.reply_to(message, f'Вы не с нами, залетайте https://t.me/{check_name}')
+    except Exception as e:
+        bot.send_message(m["arduinoev3"].id, str(e))
+
 @bot.message_handler()
 def start_message(message):
-    global m, check_name
-    if message.from_user.username in m:
-        if m[message.from_user.username].step == 0:
-            m[message.from_user.username].phone = message.text
-            m[message.from_user.username].step = 1
-            bot.send_message(m[message.from_user.username].id, f"Если хочешь получить в подарок финансовый дневник, то присоединяйся https://t.me/{check_name}")
-        else:
-            user_id = message.chat.id
-            try:
-                member = bot.get_chat_member("@" + check_name, user_id)
-                if member.status == 'member' or member.status == 'creator':
-                    m[message.from_user.username].step = 2
-                    bot.reply_to(message, 'Вы с нами, кидаю дневник')
-
-                    f = open("Финансовый дневник.pdf","rb")
-                    bot.send_document(message.chat.id,f)
-                else:
-                    bot.reply_to(message, f'Вы не с нами, залетайте https://t.me/{check_name}')
-            except Exception as e:
-                bot.send_message(m["arduinoev3"].id, str(e))
-    else:
-        bot.send_message(message.chat.id, "Тебя не в игре - введи /start")
+    bot.send_message(message.chat.id, f"Если хочешь получить в подарок финансовый дневник, то присоединяйся https://t.me/{check_name}, а потом напи /check_me")
 
 
-
-bot.infinity_polling()
+bot.polling(none_stop=True)
