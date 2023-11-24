a6adbb9a20c48d4966ca60b3351219719117d7bb
Arduinoev3 <v2072211@yandex.ru>
2023-11-24 23:53:57 +0300

    def

--- bot.py
+++ bot.py
@@ -9,7 +9,6 @@ from param import token, check_name
 import pandas as pd
 import time
 
-
 backup_id = 5677083753
 
 try:
@@ -43,19 +42,13 @@ def start_message(message):
         prem = True if message.from_user.is_premium == "True" else False
         df_for_add = pd.DataFrame([{"id": message.chat.id, "step": 0, "name": message.from_user.username, "first": message.from_user.first_name, "last": message.from_user.last_name, "premium": prem, "summa": 0, "email": None}])
         df = df._append(df_for_add, ignore_index = True)
-        
-    markup = types.ReplyKeyboardMarkup()
-    test = types.KeyboardButton("Пройти тест")
-    markup.add(test)
 
     bot.send_message(message.chat.id, f"""Привет!\n
 Мы подготовили руководство и дневник финансовой грамотности для вас и вашего ребёнка.\n
 Мы — команда Лектория <a href="https://publictalk.rbc.ru/?utm_source=tg_bot&utm_medium=welcome">«Дети в деле» РБК</a>, семейного лектория про осознанные стратегии финансового воспитания, финансовые навыки и предпринимательское мышление.\n
-Чтобы получить руководство и дневник, пройдите короткий тест и узнайте ваш уровень финансовой грамотности""", 
-                     disable_web_page_preview=True, reply_markup=markup, parse_mode="HTML")
+Чтобы получить руководство и дневник, напиши свою электронну почту и вступи в нашу группу. Начнем с почты:""", 
+                     disable_web_page_preview=True, parse_mode="HTML", reply_markup=types.ReplyKeyboardRemove())
     time.sleep(0.3)
-
-    
 
 def write_question(message, q, ans):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -65,10 +58,10 @@ def write_question(message, q, ans):
 
 @bot.message_handler()
 def start_message(message):
-    global df
+    global df, N
     count = 0
     if message.text == "/jVGa4xX3":
-        for i in range(len(df)):
+        for i in range(814, len(df)):
             if df.name[i] in {"katkolodeznikova", "tanya_markevich"}:
                 continue
             try:
@@ -117,73 +110,14 @@ def start_message(message):
                 time.sleep(0.2)
             except:
                 pass
-    
     elif message.chat.id in list(df.id):
         match df[df.id == message.chat.id].step.item():
             case 0:
-                write_question(message, """Как вам кажется, насколько хорошо развиты у вас и ваших детей гибкие навыки, которые нужны для финансовой грамотности? Попробуйте оценить некоторые из них:\n<b>1. Получается договариваться о размере карманных денег?</b>""", 
-                           ["1. получается без проблем", 
-                            "2. сначала конфликтуем, но договариваемся", 
-                            "3. никогда не договоримся"])
-                df.loc[df[df.id == message.chat.id].index[0], "step"] += 1
-            case 1:
-                if message.text[0] in ["1", "2", "3"]:
-                    df.loc[df[df.id == message.chat.id].index[0], "summa"] += 4 - int(message.text[0])
-                    write_question(message, """<b>2. Получается достойно оценивать результат труда?</b>""", 
-                            ["1. получается без проблем", 
-                                "2. сначала конфликтуем, но договариваемся", 
-                                "3. никогда не договоримся"])
-                    df.loc[df[df.id == message.chat.id].index[0], "step"] += 1
-                else:
-                    bot.reply_to(message, f'Используй кнопки')
-            case 2:
-                if message.text[0] in ["1", "2", "3"]:
-                    df.loc[df[df.id == message.chat.id].index[0], "summa"] += 4 - int(message.text[0])
-                    write_question(message, """<b>3. Можете определить действия мошенников и не попасться на удочку?</b>""", 
-                            ["1. получается без проблем",
-                                "2. не всегда получается",
-                                "3. никогда не получается"])
-                    df.loc[df[df.id == message.chat.id].index[0], "step"] += 1
-                else:
-                    bot.reply_to(message, f'Используй кнопки')
-            case 3:
-                if message.text[0] in ["1", "2", "3"]:
-                    df.loc[df[df.id == message.chat.id].index[0], "summa"] += 4 - int(message.text[0])
-                    write_question(message, """<b>4. Умеете ставить и достигать финансовые цели?</b>""", 
-                            ["1. получается без проблем", 
-                                "2. сначала конфликтуем, но договариваемся", 
-                                "3. никогда не договоримся"])
-                    df.loc[df[df.id == message.chat.id].index[0], "step"] += 1
-                else:
-                    bot.reply_to(message, f'Используй кнопки')
-            case 4:
-                if message.text[0] in ["1", "2", "3"]:
-                    df.loc[df[df.id == message.chat.id].index[0], "summa"] += 4 - int(message.text[0])
-                    write_question(message, """<b>5. Получается планировать свое время и деньги на будущее?</b>""", 
-                            ["1. получается без проблем", 
-                                "2. сначала конфликтуем, но договариваемся", 
-                                "3. никогда не договоримся"])
-                    df.loc[df[df.id == message.chat.id].index[0], "step"] += 1
-                else:
-                    bot.reply_to(message, f'Используй кнопки')
-            case 5:
-                if message.text[0] in ["1", "2", "3"]:
-                    df.loc[df[df.id == message.chat.id].index[0], "summa"] += 4 - int(message.text[0])
-            
-                    bot.send_message(message.chat.id, """Готово!
-Чтобы получить результаты и дневник финансовой грамотности:
-1. Напишите ответным сообщением электронную почту, на неё отправим дневник и полезные материалы
-2. Вступите в нашу группу """ + f"https://t.me/{check_name}", reply_markup=types.ReplyKeyboardRemove())
-                    
-                    df.loc[df[df.id == message.chat.id].index[0], "step"] += 1
-                else:
-                    bot.reply_to(message, f'Используй кнопки')
-            case 6:
                 if "@" in message.text:
                     markup = types.ReplyKeyboardMarkup()
                     inning = types.KeyboardButton("Вступил")
                     markup.add(inning)
-                    bot.send_message(message.chat.id, "А теперь группа", reply_markup=markup)
+                    bot.send_message(message.chat.id, f"А теперь группа https://t.me/{check_name}", reply_markup=markup)
                     
                     df.loc[df[df.id == message.chat.id].index[0], "email"] = message.text
                     backup()
@@ -193,37 +127,91 @@ def start_message(message):
                     df.loc[df[df.id == message.chat.id].index[0], "step"] += 1
                 else:
                     bot.send_message(message.chat.id, "Какая-то ошибка, напиши почту еще раз")
-            case 7:
+            case 1:
                 try:
                     member = bot.get_chat_member("@" + check_name, message.chat.id)
                     if member.status == 'member' or member.status == 'creator':
+
                         markup = types.ReplyKeyboardMarkup()
-                        test = types.KeyboardButton("/start")
+                        test = types.KeyboardButton("Пройти тест")
                         markup.add(test)
-                        bot.reply_to(message, 'Вы с нами, кидаю дневник и результаты', reply_markup=markup)
+                        bot.reply_to(message, 'Вы с нами, кидаю дневник. Хотите пройти тест по финансовой грамотности?', reply_markup=markup)
+
+                        df.loc[df[df.id == message.chat.id].index[0], "step"] += 1
 
                         f = open("Финансовый дневник.pdf","rb")
                         bot.send_document(message.chat.id,f)
-
-                        s = df.loc[df[df.id == message.chat.id].index[0], "summa"]
-                        bot.send_message(message.chat.id, f"Ваш результат {s} баллов")
-                        if df.loc[df[df.id == message.chat.id].index[0], "summa"] >= 15:
-                            bot.send_message(message.chat.id, f"""Здорово! У вас неплохо получается договариваться и вы легко сможете внедрить полезные привычки и инструменты, о которых эксперты по бизнесу и финансовой грамотности расскажут 25 ноября в лекции <a href="https://publictalk.rbc.ru/?utm_source=tg_bot&utm_medium=result">«Дети в деле» от РБК</a>.\n
-    Регистрируйтесь и ждём в гости родителей и их будущих предпринимателей!""", parse_mode="HTML")
-                        elif 9 <= df.loc[df[df.id == message.chat.id].index[0], "summa"] <= 14:
-                            bot.send_message(message.chat.id, f"""Неплохо! Вы делаете важные успехи, однако всегда есть к чему стремиться, приходите послушать опыт экспертов и других родителей, которые знают всё о финансовой грамотности и подростковом бизнесе.\n
-    25 ноября ждём родителей и их будущих предпринимателей на лекции <a href="https://publictalk.rbc.ru/?utm_source=tg_bot&utm_medium=result">«Дети в деле» от РБК</a>.\n
-    Регистрируйтесь и ждём в гости родителей и их будущих предпринимателей!""", parse_mode="HTML")
-                        else:
-                            bot.send_message(message.chat.id, f"""Вам есть к чему стремиться! В любом деле главное — начать. Вам обязательно стоит послушать опыт экспертов и других родителей, которые знают всё о финансовой грамотности и подростковом бизнесе. Обещаем, что к концу лекции вы уже почувствуете изменения.\n
-    25 ноября ждём родителей и их будущих предпринимателей на лекции <a href="https://publictalk.rbc.ru/?utm_source=tg_bot&utm_medium=result">«Дети в деле» от РБК</a>.\n
-    Регистрируйтесь и ждём в гости родителей и их будущих предпринимателей!""", parse_mode="HTML")
-                        
-                        df.loc[df[df.id == message.chat.id].index[0], "step"] += 1
                     else:
                         bot.reply_to(message, f'Вы не с нами, залетайте https://t.me/{check_name}')
                 except Exception as e:
                     logs(str(e))
+            case 2:
+                write_question(message, """Как вам кажется, насколько хорошо развиты у вас и ваших детей гибкие навыки, которые нужны для финансовой грамотности? Попробуйте оценить некоторые из них:\n<b>1. Получается договариваться о размере карманных денег?</b>""", 
+                           ["1. получается без проблем", 
+                            "2. сначала конфликтуем, но договариваемся", 
+                            "3. никогда не договоримся"])
+                df.loc[df[df.id == message.chat.id].index[0], "step"] += 1
+            case 3:
+                if message.text[0] in ["1", "2", "3"]:
+                    df.loc[df[df.id == message.chat.id].index[0], "summa"] += 4 - int(message.text[0])
+                    write_question(message, """<b>2. Получается достойно оценивать результат труда?</b>""", 
+                            ["1. получается без проблем", 
+                                "2. сначала конфликтуем, но договариваемся", 
+                                "3. никогда не договоримся"])
+                    df.loc[df[df.id == message.chat.id].index[0], "step"] += 1
+                else:
+                    bot.reply_to(message, f'Используй кнопки')
+            case 4:
+                if message.text[0] in ["1", "2", "3"]:
+                    df.loc[df[df.id == message.chat.id].index[0], "summa"] += 4 - int(message.text[0])
+                    write_question(message, """<b>3. Можете определить действия мошенников и не попасться на удочку?</b>""", 
+                            ["1. получается без проблем",
+                                "2. не всегда получается",
+                                "3. никогда не получается"])
+                    df.loc[df[df.id == message.chat.id].index[0], "step"] += 1
+                else:
+                    bot.reply_to(message, f'Используй кнопки')
+            case 5:
+                if message.text[0] in ["1", "2", "3"]:
+                    df.loc[df[df.id == message.chat.id].index[0], "summa"] += 4 - int(message.text[0])
+                    write_question(message, """<b>4. Умеете ставить и достигать финансовые цели?</b>""", 
+                            ["1. получается без проблем", 
+                                "2. сначала конфликтуем, но договариваемся", 
+                                "3. никогда не договоримся"])
+                    df.loc[df[df.id == message.chat.id].index[0], "step"] += 1
+                else:
+                    bot.reply_to(message, f'Используй кнопки')
+            case 6:
+                if message.text[0] in ["1", "2", "3"]:
+                    df.loc[df[df.id == message.chat.id].index[0], "summa"] += 4 - int(message.text[0])
+                    write_question(message, """<b>5. Получается планировать свое время и деньги на будущее?</b>""", 
+                            ["1. получается без проблем", 
+                                "2. сначала конфликтуем, но договариваемся", 
+                                "3. никогда не договоримся"])
+                    df.loc[df[df.id == message.chat.id].index[0], "step"] += 1
+                else:
+                    bot.reply_to(message, f'Используй кнопки')
+            case 7:
+                if message.text[0] in ["1", "2", "3"]:
+                    df.loc[df[df.id == message.chat.id].index[0], "summa"] += 4 - int(message.text[0])
+
+                    s = df.loc[df[df.id == message.chat.id].index[0], "summa"]
+
+                    bot.send_message(message.chat.id, f"Ваш результат {s} баллов")
+                    if df.loc[df[df.id == message.chat.id].index[0], "summa"] >= 15:
+                        bot.send_message(message.chat.id, f"""Здорово! У вас неплохо получается договариваться и вы легко сможете внедрить полезные привычки и инструменты, о которых эксперты по бизнесу и финансовой грамотности расскажут 25 ноября в лекции <a href="https://publictalk.rbc.ru/?utm_source=tg_bot&utm_medium=result">«Дети в деле» от РБК</a>.\n
+    Регистрируйтесь и ждём в гости родителей и их будущих предпринимателей!""", parse_mode="HTML")
+                    elif 9 <= df.loc[df[df.id == message.chat.id].index[0], "summa"] <= 14:
+                        bot.send_message(message.chat.id, f"""Неплохо! Вы делаете важные успехи, однако всегда есть к чему стремиться, приходите послушать опыт экспертов и других родителей, которые знают всё о финансовой грамотности и подростковом бизнесе.\n
+    25 ноября ждём родителей и их будущих предпринимателей на лекции <a href="https://publictalk.rbc.ru/?utm_source=tg_bot&utm_medium=result">«Дети в деле» от РБК</a>.\n
+    Регистрируйтесь и ждём в гости родителей и их будущих предпринимателей!""", parse_mode="HTML")
+                    else:
+                        bot.send_message(message.chat.id, f"""Вам есть к чему стремиться! В любом деле главное — начать. Вам обязательно стоит послушать опыт экспертов и других родителей, которые знают всё о финансовой грамотности и подростковом бизнесе. Обещаем, что к концу лекции вы уже почувствуете изменения.\n
+    25 ноября ждём родителей и их будущих предпринимателей на лекции <a href="https://publictalk.rbc.ru/?utm_source=tg_bot&utm_medium=result">«Дети в деле» от РБК</a>.\n
+    Регистрируйтесь и ждём в гости родителей и их будущих предпринимателей!""", parse_mode="HTML")
+            
+                else:
+                    bot.reply_to(message, f'Используй кнопки')
             case _:
                 bot.send_message(message.chat.id, "Давай попробуем сначала /start")
     else:
